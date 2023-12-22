5a55c7a642336b67c47885d741214a21aea7699a
mikroTesting <nowakowski.m1995@gmail.com>
2023-12-22 21:39:05 +0100

    add structure for Task03

---
+++ src/components/Breadcrumb/BreadcrumbItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import StyledBreadcrumbItem from "./BreadcrumbItem.styled";
+
+const BreadcrumbItem = (props) => {
+  return (
+    <li>
+      <StyledBreadcrumbItem>{props.children}</StyledBreadcrumbItem>
+    </li>
+  );
+};
+
+export default BreadcrumbItem;
---
+++ src/components/Breadcrumb/BreadcrumbItem.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledBreadcrumbItem = styled.a`
+  color: #007bff;
+  text-decoration: none;
+  background-color: transparent;
+`;
+
+export default StyledBreadcrumbItem;
